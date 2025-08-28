4f5ec7e9afee07ba3b5aee7a6e7781538717d3dd
SUMIT KUMAR <120464532+SumitKumar777@users.noreply.github.com>
2025-08-29 03:35:19 +0530

    init2

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
 	return (
-		<div className="text-3xl text-amber-200">
+		<div className="text-3xl text-green-500">
 			hi their from the bank frontend
 		</div>
 	);
