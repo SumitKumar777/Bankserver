c3ddc201755eef07192d3db3336c4554b0f1b12e
SUMIT KUMAR <120464532+SumitKumar777@users.noreply.github.com>
2025-09-09 04:07:01 +0530

    styled homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 
 export default function Page() {
 	return (
-		<div>
-			<h1>My Store</h1>
-
-			<h2>hi their from the bank</h2>
+		<div className="bg-black h-screen text-white">
+			<h2>Hi from the bank server</h2>
 		</div>
 	);
 }
