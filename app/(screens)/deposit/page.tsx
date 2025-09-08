48291a4de99ae02f2395d126b45f90fb1e1cd76e
SUMIT KUMAR <120464532+SumitKumar777@users.noreply.github.com>
2025-09-09 04:05:26 +0530

    styled confirmpage

--- app/(screens)/deposit/page.tsx
+++ app/(screens)/deposit/page.tsx
@@ -6,9 +6,6 @@ import {  useSearchParams } from "next/navigation";
 
 const Deposit = () => {
 
-
-
-
 	const searchParams = useSearchParams();
 	const paymentDetails = {
 		userId: searchParams.get("userId"),
@@ -40,22 +37,33 @@ const Deposit = () => {
 
 
 	return (
-		<>
-			<div className=" bg-amber-300 max-w-[500px] ">
-				<h1 className="text-4xl">Confirm Your Payment</h1>
-				<p>
-					Amount {paymentDetails.amount} will be deducted and added to your
-					paytm wallet
+		<div className="h-screen text-center ">
+			<div className="  max-w-[500px] p-4 mx-auto space-y-6 translate-y-1/2">
+				<h1 className="text-4xl font-bold">Confirm Your Payment</h1>
+				<p className="p-4 font-xl font-medium">
+					Amount{" "}
+					<span className="font-bold text-green-600">
+						{(Number(paymentDetails.amount) / 100).toString()}
+					</span>{" "}
+					will be deducted and added to your paytm wallet
 				</p>
-				<p>Paytm transaction id{paymentDetails.paytmTranx}</p>
-			
 
-				<div className="">
-					<button onClick={() => handleChoice(true)}>Complete</button>
-					<button onClick={() => handleChoice(false)}>Reject</button>
+				<div className="space-x-4 text-xl text-center ">
+					<button
+						className="border1 rounded-md bg-green-400 p-2"
+						onClick={() => handleChoice(true)}
+					>
+						Complete
+					</button>
+					<button
+						className="border1 rounded-md bg-red-400 p-2"
+						onClick={() => handleChoice(false)}
+					>
+						Reject
+					</button>
 				</div>
 			</div>
-		</>
+		</div>
 	);
 };
 
